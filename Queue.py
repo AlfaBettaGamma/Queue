0d51fb91fa26d68daf5cff316d44839a741f59eb
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-08-28 13:13:12 +0300

    first commit

---
+++ Queue.py
@@ -0,0 +1,13 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+        # инициализация хранилища данных
+    def enqueue(self, item):
+        self.queue.insert(0,item)
+        # вставка в хвост
+    def dequeue(self):
+        if self.size() < 1:
+            return None # если стек пустой
+        return self.queue.pop(0) 
+    def size(self):
+        return len(self.queue) # размер очереди
